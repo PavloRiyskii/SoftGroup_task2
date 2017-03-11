933a6c9f027a8bc9507dd8ea018aa50ae6e85341
PavloRiyskii <pavlo716@protonmail.com>
2017-03-11 13:00:43 +0200

    Update Complementary.java
    Added documnetation

--- src/Complementary.java
+++ src/Complementary.java
@@ -2,7 +2,11 @@
  * Created by pavlo on 11.03.17.
  */
 public class Complementary {
-
+    /**
+    * @param array the array of integer
+    * @param k 
+    * @return true if there are komplimentary pair
+    */
     public static boolean isComplementary (int[] array, int K) {
         int leftBound = 0, rightBound = array.length - 1;
         while(true) {
