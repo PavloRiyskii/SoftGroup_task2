f675e9f41c8b47e9ab3c86fe28f7fb29108cc737
pavlo716@protonmail.com <grig0478>
2017-03-11 12:25:59 +0200

    Initial commit

---
+++ src/Complementary.java
@@ -0,0 +1,20 @@
+/**
+ * Created by pavlo on 11.03.17.
+ */
+public class Complementary {
+
+    public static boolean isComplementary (int[] array, int K) {
+        int leftBound = 0, rightBound = array.length - 1;
+        while(true) {
+            if (leftBound == rightBound) {
+                System.out.println("There is no complementary pair");
+                return false;
+            }
+            if ((array[leftBound] + array[rightBound]) < K) leftBound++;
+            else if ((array[leftBound] + array[rightBound])> K) rightBound--;
+            else break;
+        }
+        System.out.println(K + " = " + array[leftBound] + " + " + array[rightBound] + "");
+        return true;
+    }
+}
